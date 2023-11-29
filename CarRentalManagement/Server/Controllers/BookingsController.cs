802a5405dcbfe088540dca936a2e85321e936f10
michelllim <2201198e@student.tp.edu.sg>
2023-11-29 09:12:54 +0800

    Added REST API Endpoints and Unit Of Work pattern.

---
+++ CarRentalManagement/Server/Controllers/BookingsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        //Refactored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+        public BookingsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Bookings
+        [HttpGet]
+        //Refactored
+       // public async Task<ActionResult<IEnumerable<Booking>>> GetBookings()
+        //{
+        //  if (_context.Bookings == null)
+        //  {
+        //      return NotFound();
+        //  }
+        //    return await _context.Bookings.ToListAsync();
+        //}
+        public async Task<IActionResult> GetBookings()
+        {
+            var bookings = await _unitOfWork.Bookings.GetAll();
+            return Ok(bookings);
+        }
+
+        // GET: api/Bookings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Booking>> GetBooking(int id)
+        {
+            //if (_context.Bookings == null)
+            //{
+            //    return NotFound();
+            //}
+            //  var booking = await _context.Bookings.FindAsync(id);
+            var booking = await _unitOfWork.Bookings.Get(q => q.Id == id);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(booking);
+        }
+
+        // PUT: api/Bookings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBooking(int id, Booking booking)
+        {
+            if (id != booking.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Bookings.Update(booking);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await BookingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bookings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Booking>> PostBooking(Booking booking)
+        {
+            await _unitOfWork.Bookings.Insert(booking);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetBooking", new { id = booking.Id }, booking);
+        }
+
+        // DELETE: api/Bookings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            var booking = await _unitOfWork.Bookings.Get(q=>q.Id == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Bookings.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+            
+            return NoContent();
+        }
+
+        private async Task<bool> BookingExists(int id)
+        {
+            var booking = await _unitOfWork.Bookings.Get(q=>q.Id==id);
+            return booking != null;
+        }
+    }
+}
---
+++ CarRentalManagement/Server/Controllers/ModelsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase
+    {
+        //Refactored
+        //private readonly ApplicationDbContext _context;
+        private readonly IUnitOfWork _unitOfWork;
+        public ModelsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Models
+        [HttpGet]
+        //Refactored
+       // public async Task<ActionResult<IEnumerable<Model>>> GetModels()
+        //{
+        //  if (_context.Models == null)
+        //  {
+        //      return NotFound();
+        //  }
+        //    return await _context.Models.ToListAsync();
+        //}
+        public async Task<IActionResult> GetModels()
+        {
+            var models = await _unitOfWork.Models.GetAll();
+            return Ok(models);
+        }
+
+        // GET: api/Models/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Model>> GetModel(int id)
+        {
+            //if (_context.Models == null)
+            //{
+            //    return NotFound();
+            //}
+            //  var model = await _context.Models.FindAsync(id);
+            var model = await _unitOfWork.Models.Get(q => q.Id == id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(model);
+        }
+
+        // PUT: api/Models/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModel(int id, Model model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Models.Update(model);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await ModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Models
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Model>> PostModel(Model model)
+        {
+            await _unitOfWork.Models.Insert(model);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetModel", new { id = model.Id }, model);
+        }
+
+        // DELETE: api/Models/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            var model = await _unitOfWork.Models.Get(q=>q.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            await _unitOfWork.Models.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+            
+            return NoContent();
+        }
+
+        private async Task<bool> ModelExists(int id)
+        {
+            var model = await _unitOfWork.Models.Get(q=>q.Id==id);
+            return model != null;
+        }
+    }
+}
